0e4720a8c989cea48d5c9100ee794ac4717fa88d
Ferrari <dwp393@vols.utk.edu>
2025-09-16 21:02:55 -0400

    moved dist to frontend dir

--- helloplus/webpack.config.js
+++ helloplus/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/',
+    publicPath: '/frontend/dist/',
   },
   module: {
     rules: [
