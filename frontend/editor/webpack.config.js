dde3da6d28c405b2a330218798c9d963ffcf458f
Ferrari <dwp393@vols.utk.edu>
2025-10-02 17:32:22 -0400

    bug was in webpack.js it created bundle in frointend/public/dist not frontend/dist/ so thats my bad. I'm suprised this was just now causing problem.

--- frontend/editor/webpack.config.js
+++ frontend/editor/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
     publicPath: '/frontend/dist/',
   },
