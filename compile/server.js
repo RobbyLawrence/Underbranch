3663bc55cb0ae7b8b26f1f9fb6a4cfdbf1caf434
Robby Lawrence <rlawren9@vols.utk.edu>
2025-10-04 12:12:10 -0400

    updating port numbers #13

--- compile/server.js
+++ compile/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const { spawn } = require("child_process");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(express.json({ limit: "200kb" }));
 
